d33e5a4b6adc40546c16cb74cf4d0aa4db66395c
John <jpschaef25@gmail.com>
2021-08-31 18:22:51 -0400

    started using dictionary instead of classes

--- getPrices.py
+++ getPrices.py
@@ -1,69 +1,64 @@
 import requests
 import json
-from bs4 import BeautifulSoup
+import csv
 from datetime import datetime
 from Item import Item
 
-url = 'https://prices.runescape.wiki/api/v1/osrs/latest'
-
+# Setup headers for API call so they can yell at me if we use API too heavily
 headers = {
     'User-Agent': 'TestHerbloreTracker - @chirpchirp#8969'
 }
 
+# Call API to get latest GE prices
+url = 'https://prices.runescape.wiki/api/v1/osrs/latest'
 response = requests.get(url, headers=headers)
-
 itemList = response.json()
 
-allItems= []
+# itemDict will contain the following fields: highPrice, highTime, lowPrice, lowTime, name, HighAlch
+# Access fields by itemId like this: "itemDict[itemId]['name']""
+itemDict = {}
 
+# Adds highPrice, highTime, lowPrice, lowTime to itemDict
 for item in itemList['data'].items():
-
+    # Ignores any items that haven't been traded on GE
     if item[1]['highTime'] is None or item[1]['lowTime'] is None:
         print('Fake item')
     else:
-        itemId = item[0]
-        highPrice = item[1]['high']
-        highTime = datetime.fromtimestamp(item[1]['highTime'])
-        lowPrice = item[1]['low']
-        lowTime = datetime.fromtimestamp(item[1]['lowTime'])
-        # print('ItemID=' + str(itemId) + ', HighPrice=' + str(highPrice) + ', HighTime=' + str(highTime.strftime("%m/%d/%Y, %H:%M:%S")) + ', LowPrice=' + str(lowPrice) + ', LowTime=' + str(lowTime.strftime("%m/%d/%Y, %H:%M:%S")))
+        itemId = int(item[0])
+        itemDict[itemId] = {}
+        itemDict[itemId]['highPrice'] = item[1]['high']
+        itemDict[itemId]['highTime'] = datetime.fromtimestamp(item[1]['highTime'])
+        itemDict[itemId]['lowPrice'] = item[1]['low']
+        itemDict[itemId]['lowTime'] = datetime.fromtimestamp(item[1]['lowTime'])
 
-        myItem = Item(itemId, highPrice, highTime, lowPrice, lowTime)
-
-        allItems.append(myItem)
-
-        #print(str(myItem))
-
+# Call API to get item ID, name, high alch mappings
 mapping_url = 'https://prices.runescape.wiki/api/v1/osrs/mapping'
-
 response2 = requests.get(mapping_url, headers=headers)
-
 mappingList = response2.json()
 
+# Adds item name and high alch value to itemDict
 for mapping in mappingList:
-    for item in allItems:
-        if int(mapping['id']) == int(item.itemId):
-            item.name = mapping['name']
-            if mapping.has_key('highalch'):
-                item.highAlch = mapping['highalch']
-            print(str(item))
+    itemId = int(mapping['id'])
+    # POTENTIAL ISSUE: Some ingredients may not trade on GE so they might not already be in itemDict
+    # Need to figure out a way to get values for these (i.e. amylase crystals) ... or ignore them
+    if itemId in itemDict:
+        itemDict[itemId]['name'] = mapping['name']
+        if 'highalch' in mapping:
+            itemDict[itemId]['highAlch'] = mapping['highalch']
 
-print('end')
+print('Item Dictionary Created')
 
-# for key, value in itemList['data']:
-    # print(key + ' : HighPrice=' + value['high'])
+# Read potion ingredients from CSV file
+with open('herbloreIngredients.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
 
 
 def get_highPrice(itemID):
     print('This should iterate through the list and grab the highPrice for the given itemID')
+    return itemDict[itemId]['highPrice']
 
 def get_lowPrice(itemID):
     print('This should iterate through the list and grab the lowPrice for the given itemID')
-
-
-# eh gotta parse through response to get other fields, don't remember how but can figure out
-# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-#abyssalWhip = Item('abyssal whip', 4151, 100, 1, 1, 1)
-
-#once object is created, can access fields from object like this
-#print(abyssalWhip.highPrice)
+    return itemDict[itemId]['lowPrice']
